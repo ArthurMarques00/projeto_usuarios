d35e8e775935b93bbd09f29cc5013df23b0cd9ec
Arthur <arthur.marques.clear@gmail.com>
2021-11-22 16:17:08 -0300

    first commit

---
+++ class/utils.js
@@ -0,0 +1,9 @@
+class utils{
+
+    static dateFormat(date){
+
+        return date.getDate()+'/'+(date.getMonth()+1)+'/'+date.getFullYear()+'  '+date.getHours()+':'+date.getMinutes();
+
+    }
+
+}
